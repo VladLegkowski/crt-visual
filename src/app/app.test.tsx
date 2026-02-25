0cb0021181e020be551617baba6cbabbc4ff6073
Vlad <vlad.legkovskis@gmail.com>
2026-02-25 13:00:57 +0100

    unit test fix

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { ReactFlowProvider } from '@xyflow/react';
 import { describe, it, expect } from 'vitest';
+import { DnDProvider } from '../shared/context/dnd-context';
 import { App } from './app';
 
 describe('App', () => {
   it('renders hello world message', () => {
-    render(<App />);
-    
-    const heading = screen.getByText(/main content/i);
+    render(
+      <ReactFlowProvider>
+        <DnDProvider>
+          <App />
+        </DnDProvider>
+      </ReactFlowProvider>
+    );
+
+    const heading = screen.getByText(/source/i);
     expect(heading).toBeInTheDocument();
   });
 });
